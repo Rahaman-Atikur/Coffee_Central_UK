7d184a2047d5706860617c8fc57a6e1047dbbfc4
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-12-26 23:31:56 +0000

    Done but bug need to be fixed

--- index.js
+++ index.js
@@ -27,13 +27,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const coffeeCollection = client.db('coffeeDB').collection('coffees') 
+    const coffeesCollection = client.db('coffeeDB').collection('coffees') 
+
+    app.get('/coffees',async(req,res)=>{
+      const cursor = coffeesCollection.find();
+      // const result = await cursor.toArray();
+      const result = await coffeesCollection.find().toArray();
+      res.send(result);
+
+    })
+
 
 
     app.post('/coffees', async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
-      const result = await coffeeCollection.insertOne(newCoffee);
+      const result = await coffeesCollection.insertOne(newCoffee);
       res.send(result);
 
       //
